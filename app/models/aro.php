ff5d0afc1a961dab7f2da6e4fdd75144c708fdf7
Lane Olson <laneolson@gmail.com>
2009-10-19 14:51:27 -0600

    Adding acos, aros, aros_aco models for permissions

--- app/models/aro.php
+++ app/models/aro.php
@@ -2,5 +2,14 @@
 class Aro extends AppModel
 {
 	var $name = 'Aro';
+	
+	var $hasAndBelongsToMany = array(
+		'Aco' => array(
+			'className' => 'Aco',
+			'joinTable' => 'aros_acos',
+			'foreignKey' => 'aro_id',
+			'associationForeignKey' => 'aco_id'
+		)
+	);
 }
 ?>
